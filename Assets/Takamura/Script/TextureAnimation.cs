6d88f15b9024bf0555257d6e56dad2177972abb3
yuki-takamura <g1527047@st.t-kougei.ac.jp>
2017-08-19 18:13:44 +0900

    まばたきアニメーション実装

---
+++ Assets/Takamura/Script/TextureAnimation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureAnimation: MonoBehaviour {
+
+    public Texture[] texture;
+    private Texture currentTexture;
+
+    public Material material;
+
+    private int counter = 0;
+
+    private int offset = 0;
+    private Random random;
+
+	// Use this for initialization
+	void Start () {
+
+        material.SetTexture("_MainTex", texture[0]);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        counter++;
+        if (counter >200)
+            counter = 0;
+
+        offset = Random.Range(0, 2);
+
+        if (counter < 90 + offset
+            || (counter > 100 + offset && counter < 110 + offset) 
+            || (counter > 140 + offset && counter < 150 + offset) 
+            || counter > 160 + offset)
+            material.SetTexture("_MainTex", texture[0]);
+        else
+            material.SetTexture("_MainTex", texture[1]);
+	}
+}
