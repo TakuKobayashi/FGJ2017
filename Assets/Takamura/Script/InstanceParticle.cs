f3ccdbded41f122a76662dfe97380bf9f916fb7a
yuki-takamura <g1527047@st.t-kougei.ac.jp>
2017-08-19 21:23:40 +0900

    パーティクル調整・仮表示

---
+++ Assets/Takamura/Script/InstanceParticle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstanceParticle : MonoBehaviour {
+
+    public GameObject[] particle;
+
+    private int counter = 0;
+
+	// Use this for initialization
+	void Start () {
+
+        Object.Instantiate(particle[0]);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        counter++;
+        if (counter > 500)
+        {
+            counter = 0;
+            Object.Instantiate(particle[1]);
+        }
+
+	}
+}
