f929b4944d09e4227ebec44013d237db5a7c3a9b
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-08-20 08:05:55 +0900

    FPSのスタンダードアセット入れて一部だけ導入して、マウスオーバーでカメラの向きを変えられるようにした

--- Assets/Scripts/PunchingCharacterController.cs
+++ Assets/Scripts/PunchingCharacterController.cs
@@ -1,16 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityStandardAssets.Characters.FirstPerson;
 using UnityEngine;
 
 public class PunchingCharacterController : MonoBehaviour {
+	[SerializeField] MouseLook mouseLook;
+    [SerializeField] Camera mainCamera;
 
 	// Use this for initialization
 	void Start () {
-		
+		mouseLook.Init(transform, mainCamera.transform);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        mouseLook.LookRotation(transform, mainCamera.transform);
+	}
+
+	private void FixedUpdate()
+	{
+		mouseLook.UpdateCursorLock();
 	}
 }
