9059e216af9cf62072bc7f403abead75398dc768
yuki-takamura <g1527047@st.t-kougei.ac.jp>
2017-08-19 16:26:17 +0900

    上司のパンチ待ち状態のスクリプト追加

---
+++ Assets/Takamura/Script/IdleStateMotion.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleStateMotion : MonoBehaviour {
+
+    float speed = 0.04f;
+    float angle;
+
+	// Use this for initialization
+	void Start () {
+
+        angle = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        angle += speed;
+        if (speed >= 360)
+            speed = 0;
+
+        transform.Translate(new Vector3(0, 0.002f * (float)Mathf.Sin(angle),0));
+
+	}
+}
