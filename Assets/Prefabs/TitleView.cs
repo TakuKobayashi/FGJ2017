23b30d39a1b1c88f953de4d97494e28efd2fd1d9
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-08-20 16:00:59 +0900

    TitleViewをいれました

---
+++ Assets/Prefabs/TitleView.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleView : MonoBehaviour {
+
+    public void OnStartCuttonClicked(){
+        this.gameObject.SetActive(false);
+    }
+}
