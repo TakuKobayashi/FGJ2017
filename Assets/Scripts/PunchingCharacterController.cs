32b25a63c8764529f90c28ae9a99d13c4e7f8192
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-08-20 10:08:13 +0900

    パラメータ調整は必要ですがとびます

--- Assets/Scripts/PunchingCharacterController.cs
+++ Assets/Scripts/PunchingCharacterController.cs
@@ -9,10 +9,12 @@ public class PunchingCharacterController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		mouseLook.Init(transform, mainCamera.transform);
+        OnPunchHit();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        mainCamera.transform.position = characterObject.transform.position;
         mouseLook.LookRotation(transform, mainCamera.transform);
 	}
 
@@ -28,7 +30,7 @@ public class PunchingCharacterController : MonoBehaviour {
 
     private void ShootCharacter(){
 		Rigidbody rigidbody = characterObject.AddComponent<Rigidbody>();
-		Vector3 shootVector = Util.ShootVectorFromSpeed(characterObject.gameObject.transform.position, new Vector3(100, 0, 100), 3.0f);
+		Vector3 shootVector = Util.ShootVectorFromSpeed(characterObject.gameObject.transform.position, new Vector3(10, 0, -20), 3.0f);
 		// 速さベクトルのままAddForce()を渡してはいけないぞ。力(速さ×重さ)に変換するんだ
 		Vector3 force = shootVector * rigidbody.mass;
 		rigidbody.AddForce(force, ForceMode.Impulse);
