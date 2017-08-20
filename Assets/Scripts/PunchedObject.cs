69fd20e033c813074be43f42f1c225f06aaef27f
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-08-20 12:10:26 +0900

    手の当たり判定を取るために貼り付けるClassを設置

---
+++ Assets/Scripts/PunchedObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PunchedObject : MonoBehaviour {
+    void OnCollisionEnter(Collision collision){
+        Debug.LogFormat("Enter:{0}", collision.gameObject.name);
+	}
+
+	void OnCollisionExit(Collision collision){
+		Debug.LogFormat("Exit:{0}", collision.gameObject.name);
+	}
+}
